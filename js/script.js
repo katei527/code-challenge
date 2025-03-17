5d4466ca7ef9edf3da4a9c7d3a6f7b025beb2d21
Katei <kateievans28@gmail.com>
2025-03-17 16:01:32 +0300

    challenge-accepted

--- js/script.js
+++ js/script.js
@@ -12,7 +12,9 @@ h1.style.alignItems = "center"
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "ramen-images/shoyu.jpg", rating: 5, comment: "Delicious!" },
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "ramen-images/miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "ramen-images/tonkotsu.jpg" }
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "ramen-images/tonkotsu.jpg" },
+    { id: 4, name: "Naruto Ramen", restaurant: "Ichigo", image: "ramen-images/naruto.jpg"},
+    { id: 5, name: "Kojiro Ramen", restaurant: "Ragna", image: "ramen-images/kojiro.jpg"},
 ];
 let form = document.getElementById("form")
 
